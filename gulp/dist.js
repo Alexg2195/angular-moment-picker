f6efa3643a5caef5c6e24c304517c5be27574a1f
Alan Jhonnes <aj@alanjhonnes.com>
2015-09-10 02:59:21 -0300

    test runner

--- gulp/dist.js
+++ gulp/dist.js
@@ -5,12 +5,20 @@ var $ = require('gulp-load-plugins')();
 
 var wiredep = require('wiredep');
 var concat = require('gulp-concat');
+var less = require('gulp-less');
 var _ = require('lodash');
+var path = require('path');
 
 
 module.exports = function(options) {
 
-  gulp.task('dist', ['templates'], function () {
+  gulp.task('dist-styles', [], function(){
+    return gulp.src('styles/angular-moment-picker.less')
+        .pipe(less({}))
+        .pipe(gulp.dest(options.dist + '/'));
+  });
+
+  gulp.task('dist', ['templates', 'dist-styles'], function () {
 
     return gulp.src([options.src + '/**/*.js', '!' + options.src + '/**/*.spec.js'])
       .pipe($.angularFilesort()).on('error', options.errorHandler('AngularFilesort'))
